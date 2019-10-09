7baea638025aa451f6990f3fff7618b05ff86b36
Денис Львович <den1980@gmail.com>
2019-10-09 21:15:57 +0300

    Добавил коррекции в appdelegate для запуска в iOS 12

--- BabySleep/AppDelegate.swift
+++ BabySleep/AppDelegate.swift
@@ -11,8 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if #available(iOS 13.0, *) {
+            // In iOS 13 setup is done in SceneDelegate
+        } else {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            self.window = window
+        }
+        
         return true
     }
 
