424f05c2cd04f685528ea59d86a4d82094576c54
Andrey Ponamarchuk <ponamarchuk.andrey@icloud.com>
2019-10-30 20:34:59 +0500

    Добавил метод логирования событий

--- BabySleep/Application/Common/Lib/ExceptionHandlingHelper.swift
+++ BabySleep/Application/Common/Lib/ExceptionHandlingHelper.swift
@@ -8,11 +8,19 @@
 
 import YandexMobileMetrica
 import Crashlytics
+import FirebaseAnalytics
 
 final class ExceptionHandlingHelper {
     static func reportError(message: String, excerption: NSException?) {
         assertionFailure(message)
         YMMYandexMetrica.reportError(message, exception: excerption, onFailure: nil)
         CLSLogv("%@", getVaList([message]))
+    }
+    static func logEvent(event: String, title: String, content: String) {
+        assertionFailure(content);
+        let parameters = [AnalyticsParameterItemID: "id-\(title)", AnalyticsParameterItemName: title, AnalyticsParameterContent: content]
+        
+        Analytics.logEvent(event, parameters: parameters)
+        YMMYandexMetrica.reportEvent(event, parameters: parameters, onFailure: nil)
     }
 }
