426e6e0d9a4ac480011f7d30aa0b6e7588983711
Денис Львович <den1980@gmail.com>
2019-11-20 00:42:40 +0300

    Заверстал CalendarSwitcherView, добавил таргеты.

--- BabySleep/Application/Flow/RegimeVC/Views/CalendarSwitcherView.swift
+++ BabySleep/Application/Flow/RegimeVC/Views/CalendarSwitcherView.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import PinLayout
 
 class CalendarSwitcherView: UIView {
+
+    lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "SFProText-Regular", size: 17)
+        label.textColor = .black
+
+        return label
+    }()
+
+    lazy var switchLeftButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "left_arrow"), for: .normal)
+        button.addTarget(self, action: #selector(self.switchDateBack), for: .touchUpInside)
+
+        return button
+    }()
+
+    lazy var switchRightButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "right_arrow"), for: .normal)
+        button.addTarget(self, action: #selector(self.switchDateForward), for: .touchUpInside)
+
+        return button
+    }()
 
     override init(frame: CGRect) {
 
         super.init(frame: frame)
+        self.backgroundColor = .white
 
-        self.backgroundColor = .red
         self.setupSubviews()
     }
 
@@ -24,8 +49,25 @@ class CalendarSwitcherView: UIView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
+
+        dateLabel.text = "22 октября"
+
+        dateLabel.pin.center().sizeToFit()
+        switchLeftButton.pin.left().vCenter().height(of: self).width(15%)
+        switchRightButton.pin.right().vCenter().height(of: self).width(15%)
     }
 
     private func setupSubviews() {
+        self.addSubview(dateLabel)
+        self.addSubview(switchLeftButton)
+        self.addSubview(switchRightButton)
+    }
+
+    @objc private func switchDateBack() {
+        print("switch back")
+    }
+
+    @objc private func switchDateForward() {
+        print("switch forward")
     }
 }
